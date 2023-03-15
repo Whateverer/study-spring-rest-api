71b3817dd0ace226d767d5c4684b2c1809bcb6ea
장유진 <jin94@exotech.kr>
2023-03-15 13:16:49 +0900

    Initial commit

---
+++ src/test/java/me/whatever/InflearnRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.whatever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
